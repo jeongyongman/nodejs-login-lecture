3dd364ce75b4c2d6dc0fbe256b7aa9a5906c62ed
jeongyongman <i1683u@gmail.com>
2022-01-20 09:18:20 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routers/home/home.ctrl.js
+++ app/src/routers/home/home.ctrl.js
@@ -1,15 +1,43 @@
 "use strict";
 
-const hello = (req,res)=>{
-    res.render("home/index");
+const output = {
+    hello: (req,res)=>{
+        res.render("home/index");
+    },
+    
+    hellologin: (req,res)=>{
+        res.render("home/login");
+    },
+}
+
+const users = {
+    id: ["newstep", "miero", "김팀장"],
+    psword: ["1234","1234","123456"],
 };
 
-const hellologin = (req,res)=>{
-    res.render("home/login");
+const process = {
+    login: (req,res)=>{
+        const id = req.body.id,
+            psword = req.body.psword;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        });
+    },
 };
 
 // 오브젝트 {key:value} 외부로 넘겨 주기
 module.exports = {
-    hello,
-    hellologin,
+    output,
+    process,
 };
--- app/src/routers/home/index.js
+++ app/src/routers/home/index.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 const ctrl = require("./home.ctrl");
 
-router.get("/", ctrl.hello);
-
-router.get("/login", ctrl.hellologin);
+router.get("/", ctrl.output.hello);
+router.get("/login", ctrl.output.hellologin);
+router.post("/login", ctrl.process.login);
 
 module.exports = router; // 외부로 넘겨 주기
